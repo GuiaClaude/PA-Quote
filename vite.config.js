af1090e75806b4689a4dd97f67c4ab20d4294541
GuiaClaude <guiamias@gmail.com>
2026-07-05 11:11:56 +0800

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-<<<<<<< HEAD
-  build: {
-    outDir: "dist",
-  },
-});
-=======
-  root: ".",
-  build: {
-    outDir: "dist",
-  },
-});
->>>>>>> dde34e81b740444034df67f8d62317918ab0f4ad
+export default defineConfig({plugins:[react()],build:{outDir:"dist"}});
